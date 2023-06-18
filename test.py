f9b156a2d369f862fc392aa29823a9dc184a2358
heo16072007 <traipupgmobile@gmail.com>
2023-06-18 12:41:36 +0700

    new file:   geckodriver.log 	modified:   test.py 	new file:   test2.py

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import time
 import pyautogui as pa
+import os
 
 time.sleep(5)
 
-n = 5
+n = 100
 for i in range(n):
-    pa.write("hii, anh yeu em", interval=0.1)
+    if pa.
+    pa.write("anh yeu em", interval=0.1)
     pa.press("enter")
