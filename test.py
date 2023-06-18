2bccf336d785d2ccd6ae4cedc8f738e26b18d90f
heo16072007 <traipupgmobile@gmail.com>
2023-06-18 12:33:04 +0700

    modified:   test.py

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-print("Em co yeu anhh khongg")
-print("Da coo")
+import time
+import pyautogui as pa
+
+time.sleep(5)
+
+n = 5
+for i in range(n):
+    pa.write("hii, anh yeu em", interval=0.1)
+    pa.press("enter")
