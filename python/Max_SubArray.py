b7d2047d3244ffbffd0f72696f59d2958639e40e
TraiLoveLinh <136947709+TraiLoveLinh@users.noreply.github.com>
2023-06-24 03:52:58 +0000

    renamed:    python/Max_SubaaArray.py -> python/Max_SubArray.py 	new file:   python/maxlength_subArray.py

--- python/Max_SubArray.py
+++ python/Max_SubArray.py
@@ -4,7 +4,7 @@ def calSub(A, i, j):
         S = S + A[i]
     return S
 
-#Basic case
+#Basic case O(n^3)
 def Max_Sub_Array(A):
     n = len(A)
     MaxSum = A[0]
@@ -16,9 +16,21 @@ def Max_Sub_Array(A):
                 right = j
     return MaxSum, left, right
 
-#Upgraded speed
-#def MAx_Sub
+#Upgraded speed O(n^2)
+def MAx_Sub_Array(A):
+    n = len(A)
+    MaxSum = A[0]
+    for i in range(n):
+        S = 0
+        for j in range(i, n):
+            S = S + A[j]
+            if S > MaxSum:
+                MaxSum = S
+                left = i
+                right = j
+    return MaxSum, left, right
 
 
 a = [1,2,-5,4,-10,2,3,-1,4,-2]
 print(Max_Sub_Array(a))
+print(MAx_Sub_Array(a))
---
+++ python/maxlength_subArray.py
@@ -0,0 +1,14 @@
+def maxlength_subArray(A):
+    n = len(A)
+    iMax = 0
+    Maxlen = 1
+    for i in range(n):
+        j = i
+        length = 1
+        while j < n-1 and A[j] < A[j+1]:
+            j = j + 1
+            length = length + 1
+        if length > Maxlen:
+            Maxlen = length
+            iMax = i
+    return iMax, Maxlen
