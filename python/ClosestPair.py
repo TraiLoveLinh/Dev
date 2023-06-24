af299861cbf9855244bd65f0111b27cbd1611fea
TraiLoveLinh <136947709+TraiLoveLinh@users.noreply.github.com>
2023-06-24 03:15:48 +0000

    modified:   python/ClosestPair.py

--- python/ClosestPair.py
+++ python/ClosestPair.py
@@ -1,3 +1,20 @@
+def dist(p, q):
+    from math import sqrt
+    return sqrt((p[0] - q[0])**2 + (p[1] - q[1])**2)
+
+
 def ClosestPair(P):
     n = len(P)
-    
+    p = P[0]
+    q = P[1]
+    Min = dist(p, q)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if dist(P[i], P[j]) < Min:
+                Min = dist(P[i], P[j])
+    return Min
+
+
+a = (1,1)
+b = (2,2)
+print(dist(a,b))
