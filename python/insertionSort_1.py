ee1e51c9f4f43c4b242772aee79371767e936e58
TraiLoveLinh <136947709+TraiLoveLinh@users.noreply.github.com>
2023-06-25 12:03:42 +0000

    modified:   python/insertionSort_1.py 	deleted:    python/insertionSort_2.py

--- python/insertionSort_1.py
+++ python/insertionSort_1.py
@@ -5,5 +5,34 @@ def insertionSort(A):
             A[j], A[j - 1] = A[j - 1], A[j]
             j = j - 1
 
-a = [9,5,7,4,0]
-insertionSort(a)
+def insertionSort_v2(A):
+    for i in range(1, len(A)):
+        key = A[i]
+        j = i - 1
+        while j >= 0 and A[j] > key:
+            A[j + 1] = A[j]
+            j = j - 1
+        A[j + 1] = key
+
+def insertionSort_res_v1(A, k):
+    j = k
+    if j < len(A) - 1 and A[j] > A[j+1]:
+        A[j], A[j+1] = A[j+1],A[j]
+        j = j + 1
+    if j < len(A) - 1:
+        insertionSort_res_v1(A, k+1)
+def main():
+    a = [9,5,7,4,0]
+    insertionSort_res_v1(a, 0)
+    print(a)
+
+def main_2():
+    a = [9,5,7,4,0]
+    a.sort()
+    print(a)
+    
+
+import time
+start_time = time.time()
+main_2()
+print("--- %s seconds ---" % (time.time() - start_time))
