b95b9a290afe75b605994ee073f4f011ccc814a1
TraiLoveLinh <136947709+TraiLoveLinh@users.noreply.github.com>
2023-06-25 01:52:39 +0000

    modified:   python/binarySearch.py

--- python/binarySearch.py
+++ python/binarySearch.py
@@ -49,8 +49,24 @@ def binSearch_pr2(A, K):
         else: left = mid + 1
     return -1
 
+def binarySearch(A, left, right, K):               
+    if left == right:
+        if A[left] == K:
+            return left
+        else:
+            return -1
+    else:
+        mid = (left + right) // 2
+        if A[mid] == K:
+            return mid
+        elif A[mid] > K:
+            return binarySearch(A, left, mid - 1, K)
+        else:
+            return binarySearch(A, mid + 1, right, K)
+        
 a = [1,2,3,4,5,6,7,8,9,10]
 #a = list(reversed(a))
 print(a)
 #print(binSearch_r(a, 8))
 print(binSearch_pr2(a, 8))
+print(binarySearch(a, 0, len(a) - 1, 8))
